aab1f7dcb94f558a674d8fb73cd09dc520bc1b22
hashimMH <hashimstar25@gmail.com>
2023-05-09 14:14:10 +0400

    add ex02 files

---
+++ CPP_Module_00/ex02/Account.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmohamed <hmohamed@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 14:02:02 by hmohamed          #+#    #+#             */
+/*   Updated: 2023/05/09 14:12:58 by hmohamed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+static int	getNbAccounts( void )
+{
+	
+};
+static int	getTotalAmount( void )
+{
+	
+};
+static int	getNbDeposits( void )
+{
+	
+};
+static int	getNbWithdrawals( void )
+{
+	
+};
+static void	displayAccountsInfos( void )
+{
+	
+};
+
+Account::Account( int initial_deposit )
+{
+	
+};
+
+Account::~Account( void )
+{
+	
+};
+
+void	Account::makeDeposit( int deposit )
+{
+	
+};
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	
+};
+
+int		Account::checkAmount( void ) const
+{
+	
+};
+
+void	Account::displayStatus( void ) const
+{
+	
+};
