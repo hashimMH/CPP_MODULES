4602244ae2af51b9ecb071c4ffdd8d6f061521ce
hashimMH <hashimstar25@gmail.com>
2024-03-05 15:42:05 +0400

    up

--- CPP_Module_08/ex02/MutantStack.hpp
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: hmohamed <hmohamed@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 03:27:48 by hmohamed          #+#    #+#             */
-/*   Updated: 2024/03/04 15:04:21 by hmohamed         ###   ########.fr       */
+/*   Updated: 2024/03/05 15:41:55 by hmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,18 @@ public:
     typedef typename std::stack<T>::container_type::iterator iterator;
     typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
+	MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+    MutantStack& operator=(const MutantStack& other) {
+        if (this != &other) {
+            std::stack<T>::operator=(other);
+        }
+        return *this;
+    }
+    ~MutantStack() {}
+	
     iterator begin() { return c.begin(); }
     iterator end() { return c.end(); }
-
     const_iterator begin() const { return c.begin(); }
     const_iterator end() const { return c.end(); }
 };
